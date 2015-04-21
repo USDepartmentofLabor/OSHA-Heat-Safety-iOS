98bdf63573a3d0e1c12d11dae8a55e01066c6843
skeeJay <e.j.kalafarski@gmail.com>
2015-04-21 12:24:55 -0400

    Fixed local webview links, fixes #13

--- Heat Tool/PrecautionsController.swift
+++ Heat Tool/PrecautionsController.swift
@@ -11,21 +11,22 @@ import UIKit
 class PrecautionsController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
-    var localfilePath = NSURL()
     var precautionLevel = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        webView.delegate = self
+        
+        // Get the contents of the file to load
         var localFilePath = NSBundle.mainBundle().pathForResource(precautionLevel, ofType: "html")
         var contents = NSString(contentsOfFile: localFilePath!, encoding: NSUTF8StringEncoding, error: nil)
         
-        // Set the base URL for the web view so we can access resources
-        var path = NSBundle.mainBundle().bundlePath
-        var baseUrl  = NSURL.fileURLWithPath("\(path)")
+        // Get the base URL of the file so we can access its resources
+        var baseUrl = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
         
-        webView.delegate = self
+        // Load contents into the webview
         webView.loadHTMLString(contents as! String, baseURL: baseUrl)
     }
 
@@ -34,7 +35,7 @@ class PrecautionsController: UIViewController, UIWebViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType nt: UIWebViewNavigationType) -> Bool {
         // If it's a web link
         if request.URL!.scheme == "http" {
             UIApplication.sharedApplication().openURL(request.URL!)
@@ -42,6 +43,22 @@ class PrecautionsController: UIViewController, UIWebViewDelegate {
         }
         
         // If it's a local link
+        if nt == UIWebViewNavigationType.LinkClicked {
+            // Get contents of the file to load
+            var fileName = request.URL?.lastPathComponent?.stringByDeletingPathExtension
+            var localFilePath = NSBundle.mainBundle().pathForResource(fileName, ofType: "html")
+            var contents = NSString(contentsOfFile: localFilePath!, encoding: NSUTF8StringEncoding, error: nil)
+            
+            // Set the base URL for the web view so we can access resources
+            var baseUrl  = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
+            
+            // Load contents into the webview
+            webView.loadHTMLString(contents as! String, baseURL: baseUrl)
+            
+            return false
+        }
+        
+        // If it's the initial load
         return true
     }
 
