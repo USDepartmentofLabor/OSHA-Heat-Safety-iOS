9a4fc64d5d2191fa9bae0b682e4f1ac299c7d68e
skeeJay <e.j.kalafarski@gmail.com>
2015-04-22 15:08:22 -0400

    Replaced masculine ordinal with actual degree symbol, fixes #18

--- Heat Tool/HeatIndexController.swift
+++ Heat Tool/HeatIndexController.swift
@@ -444,10 +444,10 @@ class HeatIndexController: GAITrackedViewController, CLLocationManagerDelegate, 
         self.riskButtonNow.setTitle(riskTitleString, forState: .Normal)
         if self.locationTextField.text == "" {
             self.nowLabel.text = NSLocalizedString("Calculated", comment: "Calculated Title")
-            self.feelsLikeNow.text = NSLocalizedString("Feels Like", comment: "Feels Like Title") + " \(Int(perceivedTemperature))º F"
+            self.feelsLikeNow.text = NSLocalizedString("Feels Like", comment: "Feels Like Title") + " \(Int(perceivedTemperature))° F"
         } else {
             self.nowLabel.text = NSLocalizedString("Now", comment: "Now Title")
-            self.feelsLikeNow.text = NSLocalizedString("Feels Like", comment: "Feels Like Title") + " \(Int(perceivedTemperature))º F"
+            self.feelsLikeNow.text = NSLocalizedString("Feels Like", comment: "Feels Like Title") + " \(Int(perceivedTemperature))° F"
         }
         
         // Disable current risk precautions button if minimal risk state
